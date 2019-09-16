9af0d6198d63e98b63102d180eb1b4670d8ee2be
Ahmed <ahmad.elsaidy@hotmail.com>
2019-09-16 17:29:43 +0300

    test file

---
+++ test.php
@@ -0,0 +1,62 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use Skuio\Sdk\Model\ImportProducts;
+use Skuio\Sdk\Model\Product;
+use Skuio\Sdk\Model\ProductAttribute;
+use Skuio\Sdk\Resource\Products;
+
+function createProduct( $username, $password )
+{
+  $product                 = new Product();
+  $product->sku            = '12345678911';
+  $product->name           = 'my first test';
+  $product->brand_name     = 'test brand';
+  $product->type           = 'standard';
+  $product->weight         = 15.2;
+  $product->image          = 'http://ecx.images-amazon.com/images/I/41HpVbFiyRL.jpg';
+  $product->weight_unit    = 'kg';
+  $product->height         = 5.4;
+  $product->width          = 3.4;
+  $product->length         = 2.4;
+  $product->dimension_unit = 'cm';
+
+  $variation      = new Product();
+  $variation->sku = '789465432';
+
+  $attribute          = new ProductAttribute();
+  $attribute->name    = 'size';
+  $attribute->value   = 'S';
+  $attribute->variant = true;
+
+  $variation->attributes = [ $attribute ];
+
+  $product->variations = [ $variation ];
+
+  $products = new Products( $username, $password, true );
+
+  $products = $products->store( $product );
+
+  echo $products->getCode() . '<br>';
+  print_r( $products->getResponse() );
+}
+
+function importProducts( $username, $password )
+{
+  $import           = new ImportProducts();
+  $import->csv_file = './tests/import_products_test2.csv';
+
+  $products = new Products( $username, $password, true );
+  $products = $products->import( $import );
+
+  echo $products->getCode() . '<br>';
+  print_r( $products->getResponse() );
+}
+
+$username = '49a683831249ef6a37158f1e1b86e6d5';
+$password = 'd002707ff6867c1c545adf40ab06bbdf';
+
+createProduct( $username, $password );
+echo '<br>----------------<br>';
+importProducts( $username, $password );
