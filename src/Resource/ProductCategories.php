21debd8793252d9b20a453eae97d4ef99f072ba4
Ahmed <ahmad.elsaidy@hotmail.com>
2020-01-29 17:29:20 +0200

    Product category resource

--- src/Resource/ProductCategories.php
+++ src/Resource/ProductCategories.php
@@ -3,6 +3,8 @@
 namespace Skuio\Sdk\Resource;
 
 use Exception;
+use InvalidArgumentException;
+use Skuio\Sdk\Model\Category;
 use Skuio\Sdk\Request;
 use Skuio\Sdk\Response;
 use Skuio\Sdk\Sdk;
@@ -46,5 +48,120 @@ class ProductCategories extends Sdk
     }
 
     return $this->authorizedRequest( "{$this->endpoint}/{$id}?{$request->getParams()}" );
+  }
+
+  /**
+   * Create a new category
+   *
+   * @param Category $category
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function store( Category $category )
+  {
+    return $this->authorizedRequest( $this->endpoint, $category->toJson(), self::METHOD_POST );
+  }
+
+  /**
+   * Update a category
+   *
+   * @param Category $category
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function update( Category $category )
+  {
+    if ( empty( $category->id ) )
+    {
+      throw new InvalidArgumentException( 'The "id" field is required' );
+    }
+
+    return $this->authorizedRequest( $this->endpoint . '/' . $category->id, $category->toJson(), self::METHOD_PUT );
+  }
+
+  /**
+   * Delete a category
+   *
+   * @param int $id
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function delete( int $id )
+  {
+    return $this->authorizedRequest( "{$this->endpoint}/{$id}", null, self::METHOD_DELETE );
+  }
+
+  /**
+   * Assign Category to Product
+   *
+   * @param int $categoryId
+   * @param int $productId
+   * @param int $primaryCategory
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function assignToProduct( int $categoryId, int $productId, $primaryCategory = 0 )
+  {
+    return $this->authorizedRequest( "{$this->endpoint}/{$categoryId}/assign-to-product/{$productId}?is_primary={$primaryCategory}", null, self::METHOD_GET );
+  }
+
+  /**
+   * Reassigning new category to products
+   *
+   * @param int $oldCategoryId
+   * @param int $newCategoryId
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function reassignNewCategoryToProducts( int $oldCategoryId, int $newCategoryId )
+  {
+    return $this->authorizedRequest( "{$this->endpoint}/{$oldCategoryId}/reassign-to-products/{$newCategoryId}", null, self::METHOD_GET );
+  }
+
+  /**
+   * Retrieve Product Categories for manage (in edit product when assign)
+   *
+   * @param Request|null $request
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function getProductCategoriesForManage( Request $request = null )
+  {
+    if ( ! $request )
+    {
+      $request = new Request();
+    }
+
+    return $this->authorizedRequest( "{$this->endpoint}/for-manage?" . $request->getParams(), null, self::METHOD_GET );
+  }
+
+  /**
+   * Get Product Categories tree
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function productCategoriesTree()
+  {
+    return $this->authorizedRequest( "{$this->endpoint}/tree", null, self::METHOD_GET );
+  }
+
+  /**
+   * Archive Category
+   *
+   * @param int $categoryId
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function archiveCategory(int $categoryId)
+  {
+    return $this->authorizedRequest( "{$this->endpoint}/{$categoryId}/archive", null, self::METHOD_GET );
   }
 }
