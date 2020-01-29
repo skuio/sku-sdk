28446f87fe9ad94b1ada2bdd0a8e9285d4d6fa59
Ahmed <ahmad.elsaidy@hotmail.com>
2020-01-29 15:00:21 +0200

    refactoring vendors APIs

---
+++ src/Model/Vendor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Skuio\Sdk\Model;
+
+use Skuio\Sdk\Model;
+
+/**
+ * Class Vendor
+ *
+ * @package Skuio\Sdk\Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $company_name
+ * @property string|null $primary_contact_name
+ * @property string|null $email
+ * @property string|null $purchase_order_email
+ * @property string|null $phone
+ * @property string|null $website
+ * @property float|null $leadtime
+ * @property float|null $minimum_order_quantity
+ * @property float|null $minimum_purchase_order
+ * @property bool $is_supplier
+ */
+class Vendor extends Model
+{
+
+}
--- src/Resource/Vendors.php
+++ src/Resource/Vendors.php
@@ -3,6 +3,8 @@
 namespace Skuio\Sdk\Resource;
 
 use Exception;
+use InvalidArgumentException;
+use Skuio\Sdk\Model\Vendor;
 use Skuio\Sdk\Response;
 use Skuio\Sdk\Sdk;
 
@@ -19,5 +21,36 @@ class Vendors extends Sdk
   public function get()
   {
     return $this->authorizedRequest( $this->endpoint );
+  }
+
+  /**
+   * Create a new vendor
+   *
+   * @param Vendor $vendor
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function store( Vendor $vendor )
+  {
+    return $this->authorizedRequest( $this->endpoint, $vendor->toJson(), Sdk::METHOD_POST );
+  }
+
+  /**
+   * Update a vendor
+   *
+   * @param Vendor $vendor
+   *
+   * @return Response
+   * @throws Exception
+   */
+  public function update( Vendor $vendor )
+  {
+    if ( empty( $vendor->id ) )
+    {
+      throw new InvalidArgumentException( 'The "id" field is required' );
+    }
+
+    return $this->authorizedRequest( "{$this->endpoint}/$vendor->id", $vendor->toJson(), Sdk::METHOD_PUT );
   }
 }
