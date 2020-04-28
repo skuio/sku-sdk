346577dd77835141b6e229a9cc39420f097f8dbe
Ahmed <ahmad.elsaidy@hotmail.com>
2020-04-28 09:07:30 +0300

    adding nominal_code_name for SalesOrderLine docs

--- src/Model/SalesOrderLine.php
+++ src/Model/SalesOrderLine.php
@@ -10,14 +10,15 @@ use Skuio\Sdk\Model;
  * @package Skuio\Sdk\Model
  *
  * @property int $id
- * @property string $sales_channel_line_id
- * @property int $product_id
+ * @property string|null $sales_channel_line_id
+ * @property int|null $product_id
  * @property string $description
  * @property float $amount
  * @property float $quantity
- * @property float $discount
- * @property float $tax
- * @property int $nominal_code_id
+ * @property float|null $discount
+ * @property float|null $tax
+ * @property int|null $nominal_code_id
+ * @property string|null $nominal_code_name
  */
 class SalesOrderLine extends Model
 {
